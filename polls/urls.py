cd1b612ac89090a2d9adee2ec218eb9c79721b1c
Karlo Tamayo <kreye009@ucr.edu>
2014-12-04 21:29:11 -0800

    Continued progress, though there are still some code tangles to undo before the templates can run correctly.

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import patterns, url 
+from django.conf.urls import url, patterns
 from polls import views
 #from django.contrib import admin
 
-urlpatterns = patterns ( '',
+urlpatterns = patterns('',
 
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<pk>\d+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 
-   # url(r'^polls/', include('polls.urls')),
-   # url(r'^admin/', include(admin.site.urls)),
+ #   url(r'^polls/', include('polls.urls')),
+ #   url(r'^admin/', include(admin.site.urls)),
     
 )
 
