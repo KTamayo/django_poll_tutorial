0f2af02f97c18cad9facbeb0cb121365d67af7dc
KTamayo <kreye009@ucr.edu>
2014-12-30 23:11:16 -0800

    Corrections to files though not working yet

--- polls/urls.py
+++ polls/urls.py
@@ -1,16 +1,12 @@
 from django.conf.urls import url, patterns
 from polls import views
-#from django.contrib import admin
 
 urlpatterns = patterns('',
 
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<pk>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<pk>\d+)/results/$', views.results, name='results'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
 
- #   url(r'^polls/', include('polls.urls')),
- #   url(r'^admin/', include(admin.site.urls)),
-    
 )
 
